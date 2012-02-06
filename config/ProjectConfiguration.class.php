bf0c8c59054f0abe836dda6711b7bb4e4e33eda4
waelb <waelb@megaprout.(none)>
2012-02-06 11:11:20 +0100

    Premier commit jobeet

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once '/home/waelb/dev/symfony/v1.4/lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+  }
+}
---
+++ config/lighttpd.php
@@ -0,0 +1,1 @@
+../../symfttpd/genconf
